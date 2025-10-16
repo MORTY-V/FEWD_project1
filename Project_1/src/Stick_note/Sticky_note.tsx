f388e7e7cfe5be4d2d0ac41f239d272d229275f5
Aidan Youell <112305862+aidanyouell@users.noreply.github.com>
2025-10-16 13:18:46 -0600

    Date picker no longer shows time

--- Project_1/src/Stick_note/Sticky_note.tsx
+++ Project_1/src/Stick_note/Sticky_note.tsx
@@ -11,7 +11,7 @@ export default function CreatedNote({ title, description, date, onDelete }: Crea
         <div id="note">
             <h1 id="title">{title}</h1>
                         <h2 id="description">{description}</h2>
-                        {date ? <div id="note-date">{new Date(date).toLocaleString()}</div> : null}
+                        {date ? <div id="note-date">{new Date(date).toLocaleDateString()}</div> : null}
             <button id="delete" onClick={onDelete}>Delete</button>
         </div>
     );
